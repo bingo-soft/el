c87d0f2b1c554ec729b5d2c627370fa2a424de4d
Виктор Волков <vv_volkov@mail.ru>
2023-05-11 16:20:50 +0300

    fix: fix resolveFunction to return null as well

--- src/FunctionMapper.php
+++ src/FunctionMapper.php
@@ -5,8 +5,7 @@ namespace El;
 abstract class FunctionMapper
 {
     /**
-     * Resolves the specified prefix and local name into a java.lang.Method. Returns null if no
-     * function could be found that matches the given prefix and local name.
+     * Resolves the specified prefix and local name into a function or a method
      *
      * @param prefix
      *            the prefix of the function, or "" if no prefix. For example, "fn" in
@@ -15,5 +14,5 @@ abstract class FunctionMapper
      *            the short name of the function. For example, "method" in ${fn:method()}.
      * @return \ReflectionMethod the static method to invoke, or null if no match was found.
      */
-    abstract public function resolveFunction(string $prefix, string $localName): ?\ReflectionMethod;
+    abstract public function resolveFunction(string $prefix, string $localName);
 }
